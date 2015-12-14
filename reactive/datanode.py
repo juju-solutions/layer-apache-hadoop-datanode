17aec3e9e5510e265441c5c08290ab17bf3910e2
Cory Johns <johnsca@gmail.com>
2015-12-14 00:23:10 -0500

    Refactored stopping and state reporting logic

--- reactive/datanode.py
+++ reactive/datanode.py
@@ -6,12 +6,21 @@ from charmhelpers.core import hookenv
 
 
 @when('hadoop.installed')
-@when_not('namenode.registered')
+@when_not('spec.mismatch', 'namenode.registered', 'datanode.started')
 def mark_blocked_waiting():
+    from charms.reactive.bus import get_states
+    hookenv.log('States: {}'.format(get_states().keys()))
     if not is_state('namenode.related'):
         hookenv.status_set('blocked', 'Waiting for relation to NameNode')
     else:
         hookenv.status_set('waiting', 'Waiting for NameNode')
+
+
+@when_not('namenode.available')
+def no_spec():
+    # unavailable spec can neither be confirmed nor denied
+    remove_state('spec.verified')
+    remove_state('spec.mismatch')
 
 
 @when('namenode.available')
@@ -19,31 +28,26 @@ def verify_spec(namenode):
     hadoop = get_hadoop_base()
     if utils.spec_matches(hadoop.spec(), namenode.spec()):
         set_state('spec.verified')
-        if not is_state('datanode.started'):
-            mark_blocked_waiting()
+        remove_state('spec.mismatch')
     else:
+        hookenv.log('Spec mismatch: {} != {}'.format(hadoop.spec(), namenode.spec()))
         hookenv.status_set('blocked',
                            'Spec mismatch with NameNode: {} != {}'.format(
                                hadoop.spec(), namenode.spec()))
         remove_state('spec.verified')
-        if is_state('datanode.started'):
-            hadoop = get_hadoop_base()
-            hdfs = HDFS(hadoop)
-            hdfs.stop_datanode()
-            hadoop.close_ports('datanode')
-            remove_state('datanode.started')
-
-
-@when('spec.verified', 'namenode.available')
-@when_not('namenode.registered')
-def register_datanode(namenode):
-    namenode.register_datanode()
+        set_state('spec.mismatch')
 
 
 @when('spec.verified', 'namenode.available')
 def update_etc_hosts(namenode):
     utils.update_kv_hosts(namenode.hosts_map())
     utils.manage_etc_hosts()
+
+
+@when('spec.verified', 'namenode.available')
+@when_not('namenode.registered')
+def register_datanode(namenode):
+    namenode.register_datanode()
 
 
 @when('spec.verified', 'namenode.registered')
@@ -55,4 +59,18 @@ def start_datanode(namenode):
     hdfs.start_datanode()
     hadoop.open_ports('datanode')
     set_state('datanode.started')
+
+
+@when_not('spec.verified', 'namenode.available')
+def stop_datanode():
+    if is_state('datanode.started'):
+        hadoop = get_hadoop_base()
+        hdfs = HDFS(hadoop)
+        hdfs.stop_datanode()
+        hadoop.close_ports('datanode')
+        remove_state('datanode.started')
+
+
+@when('datanode.started')
+def mark_active():
     hookenv.status_set('active', 'Ready')
